e6413e6ce4436b4fa2100180cd403d99d7980850
LeonBardoel <l.bardoel@student.tue.nl>
2026-05-21 09:37:58 +0200

    add_pose.py passes the pytest

--- src/add_pose.py
+++ src/add_pose.py
@@ -1,42 +1,20 @@
+
 import math
 import numpy as np
 import gtsam
 from gtsam.symbol_shorthand import L, X
 
-PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.1, 0.1, 0.05])
-)
-
-ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.2, 0.2, 0.1])
-)
-
-MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
-    np.array([0.05, 0.1])
-)
+PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x, y, theta)
+ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
+MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_pose(graph, initial_estimate):
+    delta_x = 2.0 * math.cos(math.pi / 4)
+    delta_y = 2.0 * math.sin(math.pi / 4)
+    delta_theta = math.pi / 2
 
-    # Define odometry measurement
-    odometry = gtsam.Pose2(2.0, 0.0, math.pi / 2)
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(delta_x, delta_y, delta_theta), ODOMETRY_NOISE))
 
-    # Add odometry factor between X3 and X4
-    graph.add(
-        gtsam.BetweenFactorPose2(
-            X(3),
-            X(4),
-            odometry,
-            ODOMETRY_NOISE
-        )
-    )
-
-    # Get previous pose
-    pose3 = initial_estimate.atPose2(X(3))
-
-    # Compose new pose
-    pose4 = pose3.compose(odometry)
-
-    # Insert X4 initial estimate
-    initial_estimate.insert(X(4), pose4)
-
+    initial_estimate.insert(X(4), gtsam.Pose2(delta_x + 4, delta_y , delta_theta))
+    
     return graph, initial_estimate
